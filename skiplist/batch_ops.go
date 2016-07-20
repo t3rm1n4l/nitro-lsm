8f91a45a3991e80d90862af275617bd4b31c69e0
Sarath Lakshman <sarathlakshman@slynux.com>
2016-07-20 13:58:18 +0530

    skiplist: add validNode check function
    Change-Id: I4ef1bcaaccd95ac09a7dfc8424c1c48de8247aae
    
    fxi
    
    Change-Id: If8244157fcca9aeda8ab96ef7b5966d8d1e5ed1c

--- skiplist/batch_ops.go
+++ skiplist/batch_ops.go
@@ -14,10 +14,21 @@ type AcceptFn func(unsafe.Pointer) bool
 
 type BatchOpCallback func(*Node, CompareFn, unsafe.Pointer, BatchOpIterator) error
 
+type ValidNodeFn func(*Node) bool
+
+func defaultValidNode(*Node) bool {
+	return true
+}
+
 func (s *Skiplist) ExecBatchOps(opItr BatchOpIterator, callb BatchOpCallback,
-	cmp CompareFn, sts *Stats) error {
+	cmp CompareFn, validNode ValidNodeFn, sts *Stats) error {
+
+	if validNode == nil {
+		validNode = defaultValidNode
+	}
+
 	err := s.execBatchOpsInner(s.head, s.tail, int(s.level), opItr,
-		cmp, callb, sts)
+		cmp, validNode, callb, sts)
 
 	if err != nil {
 		return err
@@ -31,14 +42,17 @@ func (s *Skiplist) ExecBatchOps(opItr BatchOpIterator, callb BatchOpCallback,
 }
 
 func (s *Skiplist) execBatchOpsInner(startNode, endNode *Node, level int,
-	opItr BatchOpIterator, cmp CompareFn,
+	opItr BatchOpIterator, cmp CompareFn, validNode ValidNodeFn,
 	callb BatchOpCallback, sts *Stats) (err error) {
 
 	currNode := startNode
 
 	// Iterate in the current level
 	for Compare(cmp, currNode.Item(), endNode.Item()) < 0 && opItr.Valid() {
-		rightNode, _ := currNode.getNext(level)
+		var rightNode *Node
+		for rightNode, _ = currNode.getNext(level); !validNode(rightNode); {
+			rightNode, _ = rightNode.getNext(level)
+		}
 
 		// Descend to the next level
 		if Compare(cmp, opItr.Item(), rightNode.Item()) < 0 {
@@ -48,7 +62,7 @@ func (s *Skiplist) execBatchOpsInner(startNode, endNode *Node, level int,
 				}
 			} else {
 				if err = s.execBatchOpsInner(currNode, rightNode, level-1, opItr,
-					cmp, callb, sts); err != nil {
+					cmp, validNode, callb, sts); err != nil {
 					return
 				}
 			}
